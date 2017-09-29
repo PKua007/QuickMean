48d89692e8b1fd79f12d024e2f345ff248457f21
Piotr Kubala <pkua.log@gmail.com>
2017-09-29 15:43:38 +0200

    Fully working measure input filter

--- src/pl/edu/uj/student/kubala/piotr/qm/input/MeasuresInputController.java
+++ src/pl/edu/uj/student/kubala/piotr/qm/input/MeasuresInputController.java
@@ -12,14 +12,12 @@ import pl.edu.uj.student.kubala.piotr.qm.Controller;
 import pl.edu.uj.student.kubala.piotr.qm.EDTInitializationManager;
 import pl.edu.uj.student.kubala.piotr.qm.lab.LabProject;
 import pl.edu.uj.student.kubala.piotr.qm.lab.Series;
+import pl.edu.uj.student.kubala.piotr.qm.utils.Range;
 
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.text.*;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
+import java.util.regex.Pattern;
 
 public class MeasuresInputController implements Controller
 {
@@ -44,7 +42,7 @@ public class MeasuresInputController implements Controller
     public void init() {
         JEditorPane editor = this.measuresInput.getInputPane();
         AbstractDocument doc = (AbstractDocument) editor.getDocument();
-        doc.setDocumentFilter(new MeasureDocumentFilter());
+        doc.setDocumentFilter(new MeasureDocumentFilter(measuresInput.getInputPane()));
 
         Handler handler = new Handler();
         //this.labProject.addPropertyChangeListener(handler);
@@ -58,7 +56,7 @@ public class MeasuresInputController implements Controller
         return "MeasuresInputController";
     }
 
-    private void reparseText()
+    public void reparseText()
     {
         SeriesParser parser = new SeriesParser();
         String text = measuresInput.getInputPane().getText();
@@ -83,95 +81,141 @@ public class MeasuresInputController implements Controller
         }
     }
 
-    private class MeasureDocumentFilter extends DocumentFilter
+    /**
+     * Filtr dokumentu obługujący inteligentne wpisywanie pomiarów w formacie przyjmowanym przez
+     * {@link SeriesParser}. Jego zachowanie w skrócie:
+     * <ul>
+     *     <li>wpisanie "," lub "." powoduje zamianę na "."</li>
+     *     <li>wpisanie "+-" powoduje zamianę na "±"</li>
+     *     <li>wciśnięcie " " lub ";" powoduje wstawienie pełnego odstępu "; "</li>
+     *     <li>wstawianie drugiego pełnego odstępu powoduje jedynie przesunięcie karety</li>
+     *     <li>wklejany tekst również jest przetwarzany tak, jak poprzednie podpkunty. Wynik wklejenia / wycięcia /
+     *     usunięcia zaznaczenia również również zachowa format</li>
+     *     <li>zmazanie pojedynczego znaku odstępu scala sąsiednie pomiary</li>
+     *     <li>również odstępy powstałe na brzegach wklejenia/wycięcia są w intuicyjny sposób przetwarzane</li>
+     *     <li>odstęp nie może się znaleźć na początku okna. Na końcu może być wstawiony, ale nie musi</li>
+     *     <li>wstawienie nowego pomiaru między ";" i " " zamienia rozdzielony pełny odstęp na 2 pełne odstępy</li>
+     *     <li>zmazanie całego pomiaru powoduje scalenie dwóch pełnych odstępów w 1 pełny odstęp</li>
+     * </ul>
+     */
+    public static class MeasureDocumentFilter extends DocumentFilter
     {
-        @Override
-        public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
-            System.out.println("remove [offset = " + offset + ", length = " + length + "]");
-            removeImpl(fb, offset, length);
-            SwingUtilities.invokeLater(MeasuresInputController.this::reparseText);
+        private final Pattern normalizationPattern = Pattern.compile("[; ]+");
+
+        private JTextPane pane;
+
+        public MeasureDocumentFilter(JTextPane pane) {
+            this.pane = pane;
         }
 
         @Override
-        public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
-            System.out.println("insert [string = \"" + string + "\", offset = " + offset + "]");
-            fb.insertString(offset, string, attr);
+        public void remove(FilterBypass fb, int offset, int length) throws BadLocationException
+        {
+            replace(fb, offset, length, "", null);
+        }
+
+        @Override
+        public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException
+        {
+            replace(fb, offset, 0, string, attr);
         }
 
         @Override
         public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
-            System.out.println("replace [text = \"" + text + "\", offset = " + offset + ", length = " + length + "]");
-            replaceImpl(fb, offset, length, text, attrs);
-            SwingUtilities.invokeLater(MeasuresInputController.this::reparseText);
-        }
+            Document document = fb.getDocument();
+            String originalText = document.getText(0, document.getLength());
 
-        private void removeImpl(FilterBypass fb, int offset, int length) throws BadLocationException {
-            Document doc = fb.getDocument();
-            String content = doc.getText(0, doc.getLength());
-
-            if (length == 1) {
-                switch (content.charAt(offset)) {
-                    case ';':
-                        if (offset + 1 <= content.length() && content.charAt(offset + 1) == ' ') {
-                            fb.remove(offset, 2);
-                            return;
-                        }
-                        break;
-                    case ' ':
-                        if (offset > 0 && content.charAt(offset - 1) == ';') {
-                            fb.remove(offset - 1, 2);
-                            return;
-                        }
-                        break;
-                    default:
-                        if ((offset == 0 || (offset > 2 && content.charAt(offset - 1) == ' '))
-                                && offset < content.length() - 2 && "; ".equals(content.substring(offset + 1, offset + 3))) {
-                            fb.remove(offset, 3);
-                            return;
-                        }
-                        break;
-                }
+            // Inserting minus preceded by plus - change to plus-minus
+            if (length == 0 && "-".equals(text) && offset > 0 && originalText.charAt(offset - 1) == '+') {
+                fb.replace(offset - 1, 1, "±", attrs);
+                return;
             }
 
-            fb.remove(offset, length);
+            StringBuilder replaceSimulation = new StringBuilder(originalText);
+            String initialReplace = selectionTouchesSpaces(originalText, offset, length)
+                    ? normalizeSpaces(" " + text + " ")
+                    : normalizeSpaces(text);
+            replaceSimulation.replace(offset, offset + length, initialReplace);
+
+            Range toReplaceRange = getExpandedOnSpacesRange(replaceSimulation, offset, initialReplace.length());
+            String finalReplace = toReplaceRange.cutSubstringExclusive(replaceSimulation);
+            finalReplace = normalizeSurroundingSpaces(finalReplace, offset);
+            finalReplace = replaceCommas(finalReplace);
+            finalReplace = replacePlusMinus(finalReplace);
+
+            Range replacedRange = getExpandedOnSpacesRange(originalText, offset, length);
+            fb.replace(replacedRange.getBeg(), replacedRange.getLengthExclusive(), finalReplace, attrs);
+
+            // Original insertion didn't end with space, but computed insertion does - user doesn't intend to move caret
+            // to next measure, so it should be corrected
+            if (!textEndsWithSpace(text) && textEndsWithSpace(finalReplace))
+                pane.getCaret().setDot(replacedRange.getBeg() + finalReplace.length() - 2);
         }
 
-        private void replaceImpl(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
-            Document doc = fb.getDocument();
-            String content = doc.getText(0, doc.getLength());
-            JEditorPane editorPane = measuresInput.getInputPane();
+        private boolean textEndsWithSpace(String text) {
+            return text.endsWith(" ") || text.endsWith(";");
+        }
 
-            if (",".equals(text) || ".".equals(text))
-                text = ".";
+        private String replacePlusMinus(String text) {
+            return text.replace("+-", "±");
+        }
 
-            if (length == 0) {
-                if ("-".equals(text)) {
-                    if (offset > 0 && content.charAt(offset - 1) == '+') {
-                        fb.replace(offset - 1, 1, "±", null);
-                        return;
-                    }
-                }
+        private String replaceCommas(String text) {
+            return text.replace(',', '.');
+        }
 
-                if (";".equals(text) || " ".equals(text)) {
-                    if (offset == 0)
-                        return;
-                    else if (offset < content.length() && (content.charAt(offset) == ';' || content.charAt(offset) == ' '))
-                        fb.replace(offset, 1, content.substring(offset, offset + 1), null);
-                    else if (content.charAt(offset - 1) == ';' || content.charAt(offset - 1) == ' ')
-                        return;
-                    else
-                        fb.replace(offset, 0, "; ", null);
-                    return;
-                } else {
-                    if (offset < content.length() && content.charAt(offset) == ' ') {
-                        fb.replace(offset, length, " " + text + ";", attrs);
-                        editorPane.getCaret().setDot(offset + 2);
-                        return;
-                    }
-                }
-            }
+        private String normalizeSpaces(String text) {
+            return normalizationPattern.matcher(text).replaceAll("; ");
+        }
 
-            fb.replace(offset, length, text, attrs);
+        private String normalizeSurroundingSpaces(String text, int selectionStart) {
+            // Fast bypass
+            if (text.length() == 0)
+                return text;
+            if (text.length() == 1 && isSpace(text.charAt(0)))
+                return "";
+
+            // Trim single spaced on the ends. Normalize longer spaces on the ends. Delete space on the beginning
+            // if selection starts at 0 index - we don't need space on series input beginning.
+            Range nonSpaceRange = getShrunkOnSpacesRange(text, 0, text.length());
+            String trim = nonSpaceRange.cutSubstringExclusive(text);
+            if (selectionStart > 0 && nonSpaceRange.getBeg() > 1)
+                trim = "; " + trim;
+            if (nonSpaceRange.getEnd() < text.length())
+                trim = trim + "; ";
+            return trim;
+        }
+
+        private Range getShrunkOnSpacesRange(CharSequence text, int offset, int length) {
+            int beg = offset, end = beg + length;
+            while (beg < text.length() && isSpace(text.charAt(beg)))
+                beg++;
+            while (end > beg && isSpace(text.charAt(end - 1)))
+                end--;
+            return new Range(beg, end);
+        }
+
+        private boolean isSpace(char c) {
+            return c == ' ' || c == ';';
+        }
+
+        private Range getExpandedOnSpacesRange(CharSequence text, int offset, int length) {
+            int beg = offset, end = offset + length;
+            while (beg > 0 && isSpace(text.charAt(beg - 1)))    // expand on the left
+                beg--;
+            while (end < text.length() && isSpace(text.charAt(end)))   // expand on the right
+                end++;
+            return new Range(beg, end);
+        }
+
+        private boolean selectionTouchesSpaces(String text, int offset, int length) {
+            return text.length() >= 2       // length at least 2 required for touching
+                    && offset != 0 && isSpace(text.charAt(offset - 1))      // left end touches?
+                    && offset + length < text.length() && isSpace(text.charAt(offset + length));  // right end touches?
+        }
+
+        public JTextPane getPane() {
+            return pane;
         }
     }
 }
---
+++ src/pl/edu/uj/student/kubala/piotr/qm/tests/MeasureDocumentFilterTest.java
@@ -0,0 +1,469 @@
+// QuickMean - MeasureDocumentFilterTest.java
+//---------------------------------------------------------------------
+// [opis pliku]
+//---------------------------------------------------------------------
+// Utworzono 13:49 28.09.2017 w IntelliJ IDEA
+// (C)PKua, wszystkie prawa zastrzeżone
+//---------------------------------------------------------------------
+
+package pl.edu.uj.student.kubala.piotr.qm.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import pl.edu.uj.student.kubala.piotr.qm.input.MeasuresInputController;
+
+import javax.swing.*;
+import javax.swing.text.*;
+
+
+class MeasureDocumentFilterTest
+{
+    private Bypass bypass;
+    private JTextPane pane = new JTextPane();
+    private MeasuresInputController.MeasureDocumentFilter filter = new MeasuresInputController.MeasureDocumentFilter(pane);
+
+    class Bypass extends DocumentFilter.FilterBypass
+    {
+        private String text;
+        private int replaceOffset;
+        private int replaceLength;
+        private String replaceString;
+
+        public Bypass(String text) throws BadLocationException {
+            this.text = text;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public int getReplaceOffset() {
+            return replaceOffset;
+        }
+
+        public int getReplaceLength() {
+            return replaceLength;
+        }
+
+        public String getReplaceString() {
+            return replaceString;
+        }
+
+        @Override
+        public Document getDocument() {
+            return pane.getDocument();
+        }
+
+        @Override
+        public void remove(int offset, int length) throws BadLocationException {
+
+            throw new RuntimeException("Shouldn't use remove");
+        }
+
+        @Override
+        public void insertString(int offset, String string, AttributeSet attr) throws BadLocationException {
+            throw new RuntimeException("Shouldn't use insertString");
+        }
+
+        @Override
+        public void replace(int offset, int length, String string, AttributeSet attrs) throws BadLocationException {
+            if (length < 0)
+                throw new IllegalArgumentException("length < 0");
+            if (offset < 0)
+                throw new BadLocationException(text, offset);
+            if (offset + length > text.length())
+                throw new BadLocationException(text, offset + length);
+            StyledDocument document = pane.getStyledDocument();
+            StringBuilder builder = new StringBuilder(text);
+            builder.replace(offset, offset + length, string);
+            document.remove(offset, length);
+            document.insertString(offset, string, attrs);
+            text = builder.toString();
+            assertEquals(text, document.getText(0, document.getLength()));
+        }
+
+    }
+
+    private void setupBypass(String text) throws BadLocationException {
+        pane.setText(text);
+        bypass = new Bypass(text);
+    }
+
+    private void replace(int offset, int length, String text, boolean caretLeft) throws BadLocationException {
+        Caret caret = pane.getCaret();
+        if (caretLeft) {
+            caret.setDot(offset + length);
+            caret.moveDot(offset);
+        } else {
+            caret.setDot(offset);
+            caret.moveDot(offset + length);
+        }
+        filter.replace(bypass, offset, length, text, null);
+    }
+
+    private void replace(int offset, int length, String text) throws BadLocationException {
+        replace(offset, length, text, true);
+    }
+
+    @Test
+    void insertDigitToEmpty() throws BadLocationException {
+        setupBypass("");
+        replace(0, 0, "5");
+        expectText("5");
+    }
+
+    private void expectText(String expected) {
+        assertEquals(expected, bypass.getText());
+    }
+
+    @Test
+    void insertDigitToNonEmptyOnTheEnd() throws BadLocationException {
+        setupBypass("5");
+        replace(1, 0, "6");
+        expectText("56");
+    }
+
+    @Test
+    void insertDigitToNonEmptyWithSemicolonOnTheEnd() throws BadLocationException {
+        setupBypass("5; ");
+        replace(1, 0, "6");
+        expectText("56; ");
+    }
+
+    @Test
+    void insertDigitToNonEmptyWithSemicolonOnTheBeginning() throws BadLocationException {
+        setupBypass("5; ");
+        replace(1, 0, "6");
+        expectText("56; ");
+    }
+
+    @Test
+    void insertDigitInTheMiddle() throws BadLocationException {
+        setupBypass("56; ");
+        replace(1, 0, "7");
+        expectText("576; ");
+    }
+
+    @Test
+    void insertSpaceAfter() throws BadLocationException {
+        setupBypass("56");
+        replace(2, 0, " ");
+        expectText("56; ");
+
+        setupBypass("56");
+        replace(2, 0, ";");
+        expectText("56; ");
+    }
+
+    @Test
+    void insertMoreSpacesAfter() throws BadLocationException {
+        setupBypass("56");
+        replace(2, 0, "; ;; ; ");
+        expectText("56; ");
+    }
+
+    @Test
+    void insertMoreDigits() throws BadLocationException {
+        setupBypass("56; 78; 90; ");
+        replace(4, 0, "122");
+        expectText("56; 12278; 90; ");
+    }
+
+    @Test
+    void insertBetweenSpaces() throws BadLocationException {
+        setupBypass("12; 34; 56; ");
+        replace(3, 0, "9");
+        expectText("12; 9; 34; 56; ");
+
+        setupBypass("12; 34; 56; ");
+        replace(3, 0, "098");
+        expectText("12; 098; 34; 56; ");
+    }
+
+    @Test
+    void insertBetweenSpacesWithSpaces() throws BadLocationException {
+        setupBypass("12; 34; 56; ");
+        replace(3, 0, "; 098;");
+        expectText("12; 098; 34; 56; ");
+    }
+
+    @Test
+    void insertInMeasureWithSpaces() throws BadLocationException {
+        setupBypass("123; 346; 789; ");
+        replace(5, 0, ";098;");
+        expectText("123; 098; 346; 789; ");
+
+        setupBypass("123; 346; 789; ");
+        replace(6, 0, ";098;");
+        expectText("123; 3; 098; 46; 789; ");
+
+        setupBypass("123; 346; 789; ");
+        replace(6, 0, ";098");
+        expectText("123; 3; 09846; 789; ");
+    }
+
+    @Test
+    void insertWithSpacesAnZeroIndex() throws BadLocationException {
+        setupBypass("123; 346; 789; ");
+        replace(0, 0, "; 098;");
+        expectText("098; 123; 346; 789; ");
+    }
+
+    @Test
+    void travelOnSpace() throws BadLocationException {
+        setupBypass("12; 34; ");
+
+        replace(2, 0, ";");
+        expectText("12; 34; ");
+        replace(2, 0, " ");
+        expectText("12; 34; ");
+        replace(3, 0, ";");
+        expectText("12; 34; ");
+        replace(3, 0, " ");
+        expectText("12; 34; ");
+    }
+
+    @Test
+    void repairBrokenSpace() throws BadLocationException {
+        setupBypass("12;34; ");
+        replace(3, 0, ";");
+        expectText("12; 34; ");
+
+        setupBypass("12 34; ");
+        replace(3, 0, ";");
+        expectText("12; 34; ");
+
+        setupBypass("12; ;; 34; ");
+        replace(3, 0, ";");
+        expectText("12; 34; ");
+
+        setupBypass("34;");
+        replace(3, 0, ";");
+        expectText("34; ");
+    }
+
+    @Test
+    void spaceAtBeginningShouldNotBeInserted() throws BadLocationException {
+        setupBypass("12; 34; ");
+        replace(0, 0, " ");
+        expectText("12; 34; ");
+
+        setupBypass("12; 34; ");
+        replace(0, 0, ";");
+        expectText("12; 34; ");
+    }
+
+    @Test
+    void divideMeasures() throws BadLocationException {
+        setupBypass("12; 34; ");
+        replace(5, 0, " ");
+        expectText("12; 3; 4; ");
+
+        setupBypass("12; 34; ");
+        replace(5, 0, ";");
+        expectText("12; 3; 4; ");
+    }
+
+    @Test
+    void divideMeasuresWithLotsOfSpaces() throws BadLocationException {
+        setupBypass("12; 34; ");
+        replace(5, 0, ";;  ;; ; ;; ; ;");
+        expectText("12; 3; 4; ");
+    }
+
+    @Test
+    void deleteSpace() throws BadLocationException {
+        setupBypass("12; 34; ");
+        replace(2, 1, "");
+        expectText("1234; ");
+
+        setupBypass("12; 34; ");
+        replace(3, 1, "");
+        expectText("1234; ");
+
+        setupBypass("12; 34; ");
+        replace(2, 2, "");
+        expectText("1234; ");
+    }
+
+    @Test
+    void deleteSpaceAtTheEnd() throws BadLocationException {
+        setupBypass("12; 34; ");
+        replace(6, 1, "");
+        expectText("12; 34");
+
+        setupBypass("12; 34; ");
+        replace(7, 1, "");
+        expectText("12; 34");
+
+        setupBypass("12; 34; ");
+        replace(6, 2, "");
+        expectText("12; 34");
+    }
+
+    @Test
+    void deleteToCombineMeasures() throws BadLocationException {
+        setupBypass("123; 456; 789; ");
+        replace(1, 5, "");
+        expectText("156; 789; ");
+    }
+
+    @Test
+    void deleteOneDigitFromMeasure() throws BadLocationException {
+        setupBypass("123; 456; 890; ");
+        replace(2, 1, "");
+        expectText("12; 456; 890; ");
+    }
+
+    @Test
+    void deleteTheOnlyDigitFromMeasure() throws BadLocationException {
+        setupBypass("123; 4; 567; ");
+        replace(5, 1, "");
+        expectText("123; 567; ");
+    }
+
+    @Test
+    void deleteTheOnlyDigitFromLastMeasure() throws BadLocationException {
+        setupBypass("123; 567; 4; ");
+        replace(10, 1, "");
+        expectText("123; 567; ");
+
+        setupBypass("123; 567; 4");
+        replace(10, 1, "");
+        expectText("123; 567; ");
+
+    }
+
+    @Test
+    void deleteWholeMeasure() throws BadLocationException {
+        setupBypass("123; 456; 789; ");
+        replace(5, 3, "");
+        expectText("123; 789; ");
+
+        setupBypass("123; 456; 789; ");
+        replace(10, 3, "");
+        expectText("123; 456; ");
+    }
+
+    @Test
+    void deleteWholeMeasureWithSomeSpace() throws BadLocationException {
+        setupBypass("123; 456; 789; ");
+        replace(3, 5, "");
+        expectText("123; 789; ");
+
+        setupBypass("123; 456; 789; ");
+        replace(4, 5, "");
+        expectText("123; 789; ");
+
+        setupBypass("123; 456; 789; ");
+        replace(5, 5, "");
+        expectText("123; 789; ");
+
+        setupBypass("123; 456; 789; ");
+        replace(4, 4, "");
+        expectText("123; 789; ");
+
+        setupBypass("123; 456; 789; ");
+        replace(5, 4, "");
+        expectText("123; 789; ");
+    }
+
+    @Test
+    void replaceMeasureWithAnother() throws BadLocationException {
+        setupBypass("123; 456; 789; ");
+        replace(5, 3, "0e5");
+        expectText("123; 0e5; 789; ");
+    }
+
+    @Test
+    void replaceSpaceWithSpace() throws BadLocationException {
+        setupBypass("123; 456; 789; ");
+        replace(4, 1, ";  ; ");
+        expectText("123; 456; 789; ");
+        replace(3, 2, "  ; ;;");
+        expectText("123; 456; 789; ");
+        replace(13, 2, ";;; ");
+        expectText("123; 456; 789; ");
+    }
+
+    @Test
+    void replaceMeasureWithSpace() throws BadLocationException {
+        setupBypass("123; 456; 789; ");
+        replace(5, 3, ";;; ");
+        expectText("123; 789; ");
+    }
+
+    @Test
+    void replaceFewMeasuresWithSpacesWithFewMeasuresWithSpaces() throws BadLocationException {
+        setupBypass("123; 456; 789; ");
+        replace(4, 7, " 456; 32; ");
+        expectText("123; 456; 32; 89; ");
+
+        setupBypass("123; 456; 789; ");
+        replace(4, 7, "456; 32 ");
+        expectText("123456; 32; 89; ");
+
+        setupBypass("123; 456; 789; ");
+        replace(4, 7, " 456; 32");
+        expectText("123; 456; 3289; ");
+    }
+
+    @Test
+    void insertMeasuresWithMessedUpSpaces() throws BadLocationException {
+        setupBypass("123");
+        replace(3, 0, ";; ;456;789;;; 000;; ");
+        expectText("123; 456; 789; 000; ");
+    }
+
+    @Test
+    void insertEverythingMessedUpAsMuchAsItCanBe() throws BadLocationException {
+        setupBypass("123; 456; 789; ");
+        replace(0, 6, ";0e4; ;;; 00;; 11");
+        expectText("0e4; 00; 1156; 789; ");
+    }
+
+    @Test
+    void insertWithPlusMinus() throws BadLocationException {
+        setupBypass("");
+        replace(0, 0, "2+-3");
+        expectText("2±3");
+    }
+
+    @Test
+    void insertProducingPlusMinusOnTheSide() throws BadLocationException {
+        setupBypass("23+23");
+        replace(3, 0, "-3");
+        expectText("23+-323");
+    }
+
+    @Test
+    void producePlusMinus() throws BadLocationException {
+        setupBypass("23+; 5; ");
+        replace(3, 0, "-");
+        expectText("23±; 5; ");
+    }
+
+    @Test
+    void insertComma() throws BadLocationException {
+        setupBypass("2");
+        replace(1, 0, ",");
+        expectText("2.");
+    }
+
+    @Test
+    void insertMeasuresWithCommas() throws BadLocationException {
+        setupBypass("");
+        replace(0, 0, "1,3; 4,5; ");
+        expectText("1.3; 4.5; ");
+    }
+
+    /*@Test
+    void spaceMovingCaret() throws BadLocationException {
+        setupBypass("56; ");
+        replace(0, 0, "15; ");
+        expectText("15; 56; ");
+        assertEquals(4, pane.getCaret().getDot());
+        assertEquals(4, pane.getCaret().getMark());
+    }*/
+}
